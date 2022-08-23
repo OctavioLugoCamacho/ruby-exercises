6939acf369c9391543b8642eaaef587eba2c9ed2
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-08-23 11:38:47 -0500

    Add functions and unit tests for day 6B

--- lights.rb
+++ lights.rb
@@ -14,8 +14,28 @@ class Lights
             @lights[index] = 1
           elsif command["move"] == "turn off"
             @lights[index] = 0
-          elsif command["move"] == "toggle"
+          else
             @lights[index] = @lights[index] === 0 ? 1 : 0
+          end
+        end
+      end
+    end
+    return @lights.sum()
+  end
+
+  def day6b
+    @s.each do |s|
+      command = commands(s)
+      for x in command["x1"].to_i..command["x2"].to_i
+        for y in command["y1"].to_i..command["y2"].to_i
+          index = 1000 * x + y
+          case command["move"]
+          when "turn on"
+            @lights[index] += 1
+          when "turn off"
+            @lights[index] = @lights[index] === 0 ? 0 : @lights[index] - 1
+          else
+            @lights[index] += 2
           end
         end
       end
@@ -33,8 +53,11 @@ end
 if __FILE__ == $0
   file = []
   File.foreach("lights.txt") { |line| file << line }
-  ary = Array.new(1000 * 1000, 0)
-  lights = Lights.new(file, ary)
-  puts lights.day6a
+  ary1 = Array.new(1000 * 1000, 0)
+  ary2 = Array.new(1000 * 1000, 0)
+  lights1 = Lights.new(file, ary1)
+  lights2 = Lights.new(file, ary2)
+  puts lights1.day6a
+  puts lights2.day6b
 end
 
--- lights_spec.rb
+++ lights_spec.rb
@@ -16,4 +16,20 @@ describe Lights do
 
     lights.day6a.should == 1000
   end
+
+  it "would increase the total brightness by 1" do
+    ary = Array.new(1000 * 1000, 0)
+    command = ["turn on 0,0 through 0,0"]
+    lights = Lights.new(command, ary)
+
+    lights.day6b.should == 1
+  end
+
+  it "would increase the total brightness by 2000000" do
+    ary = Array.new(1000 * 1000, 0)
+    command = ["toggle 0,0 through 999,999"]
+    lights = Lights.new(command, ary)
+
+    lights.day6b.should == 2000000
+  end
 end
